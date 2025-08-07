a2bc6c35fd4ebd35c0b26d8634d3e763cbfde137
Edson Juliano Drosdeck <edson@policorp.com.br>
2025-08-07 15:15:32 +0000

    Adicionado pacote salsa

---
+++ gufw/gufw/controller.py
@@ -0,0 +1,26 @@
+# Gufw - https://costales.github.io/projects/gufw/
+# Copyright (C) 2008-2023 Marcos Alvarez Costales https://costales.github.io
+#
+# Gufw is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gufw is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gufw; if not, see http://www.gnu.org/licenses for more
+# information.
+
+from gufw.model.frontend import Frontend
+
+
+class Controller:
+    def __init__(self):
+        self.frontend = Frontend()
+    
+    def get_frontend(self):
+        return self.frontend
